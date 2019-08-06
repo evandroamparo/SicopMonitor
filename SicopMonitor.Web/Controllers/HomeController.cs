a73a5586bab12ff141316a056dc7807c1ac3513e
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2019-08-06 08:35:39 -0300

    Commit inicial

---
+++ SicopMonitor.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Sicop.Client;
+using Sicop.Client.Http;
+
+namespace SicopMonitor.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHTTPClient _httpClient;
+
+        public HomeController(IConfiguration configuration, IHTTPClient httpClient)
+        {
+            _configuration = configuration;
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cliente = new SicopClient(_configuration["UrlBase"].ToString(), _httpClient);
+            return View(model: await cliente.AtualizarStatusAsync());
+        }
+    }
+}
