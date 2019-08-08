b4e5735db871948249d13bf936db34be9b88ed42
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2019-08-08 14:12:53 -0300

    Data e hora pt-BR

--- SicopMonitor.Web/Program.cs
+++ SicopMonitor.Web/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace SicopMonitor.Web
     {
         public static void Main(string[] args)
         {
+            CultureInfo.CurrentCulture = new CultureInfo("pt-BR");
             CreateWebHostBuilder(args).Build().Run();
         }
 
